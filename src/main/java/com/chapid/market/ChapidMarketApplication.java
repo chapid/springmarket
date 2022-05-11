85d2a745da853b64a9844af5727211b61f0cc3e0
chapid <chapid9@gmail.com>
2022-05-10 21:45:47 -0500

    inicio de proyecto springboot gradle

---
+++ src/main/java/com/chapid/market/ChapidMarketApplication.java
@@ -0,0 +1,13 @@
+package com.chapid.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapidMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapidMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chapid/market/ChapidMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapid.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChapidMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
